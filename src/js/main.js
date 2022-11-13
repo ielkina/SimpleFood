17e58290c016b9d575d03357148083d4382ad09f
Ирина <ielkinairinka@gmail.com>
2022-11-13 17:20:49 +0300

    added previews section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 $(function () {
+  $('.reviews-slider').slick({
+    dots: true,
+    // autoplay: true,
+    // fade: true,
+    autoplaySpeed: 6000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-prev__icon" style="width: 34px; height: 34px;"><use class="slick-prev__icon-use" xlink:href="img/icons/sprite.svg#icon-prev-arrow"></use></svg></button >',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slick-next__icon" style="width: 34px; height: 34px;"><use class="slick-next__icon-use" xlink:href="img/icons/sprite.svg#icon-next-arrow"></use></svg></button>'
+  });
 
   var containerEl = document.querySelector('.popular-food');
   var mixer = mixitup(containerEl);
