0032ce5101526202fbfbf4eba64fa47904fa424b
Ирина <ielkinairinka@gmail.com>
2023-01-22 19:09:51 +0300

    adaptiv hedar hero popular delivery  rest gulp buildaurant

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,31 @@
 "use strict";
 $(function () {
+  $(".restaurant__list").slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 3800,
+        settings: "unslick",
+      },
+      {
+        breakpoint: 675,
+        settings: "slick",
+      },
+    ],
+  });
   $(".reviews-slider").slick({
     dots: true,
     arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     prevArrow:
       '<button type="button" class="slick-arrow__prev"><svg class="slick-arrow__icon"><use xlink:href="img/svg/sprite.svg#icon-prev-arrow"></use></svg></button >',
     nextArrow:
       '<button type="button" class="slick-arrow__next"><svg class="slick-arrow__icon"><use xlink:href="img/svg/sprite.svg#icon-prev-arrow"></use></svg></button>',
   });
-  // var mixer = mixitup(".popular__filter");
-    var containerEl = document.querySelector(".popular-food");
-    var mixer = mixitup(containerEl);
+  var containerEl = document.querySelector(".popular-food");
+  var mixer = mixitup(containerEl);
 });
