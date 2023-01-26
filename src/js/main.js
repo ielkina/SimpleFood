f40c02e85e333194d0ac4430a4674c97f05dd6b0
Iryna <ielkinairinka@gmail.com>
2023-01-26 18:45:33 +0300

    fixed code

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 "use strict";
 // alert("hello");
-$(document).ready(function () {
-  $(window).scroll(function () {
-    //липкая шапка
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 57) {
-      $("body").addClass("header-fixed");
-    } else {
-      $("body").removeClass("header-fixed");
-    }
-  });
-});
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     //липкая шапка
+//     var scrollTop = $(window).scrollTop();
+//     if (scrollTop > 57) {
+//       $("body").addClass("header-fixed");
+//     } else {
+//       $("body").removeClass("header-fixed");
+//     }
+//   });
+// });
 $(function () {
   //слайдер ресторанов
   $(".restaurant__list").slick({
@@ -41,14 +41,14 @@ $(function () {
       '<button type="button" class="slick-arrow__next"><svg class="slick-arrow__icon"><use xlink:href="img/svg/sprite.svg#icon-prev-arrow"></use></svg></button>',
   });
   //бургер меню
-  $(".burger").on("click", function () {
-    $(".burger-menu").addClass("burger-menu__active");
-    $("body").addClass("lock");
-  });
-  $(".burger-menu__close").on("click", function () {
-    $(".burger-menu").removeClass("burger-menu__active");
-    $("body").removeClass("lock");
-  });
+  // $(".burger").on("click", function () {
+  //   $(".burger-menu").addClass("burger-menu__active");
+  //   $("body").addClass("lock");
+  // });
+  // $(".burger-menu__close").on("click", function () {
+  //   $(".burger-menu").removeClass("burger-menu__active");
+  //   $("body").removeClass("lock");
+  // });
   //фильтр продуктов
   var containerEl = document.querySelector(".popular-food");
   var mixer = mixitup(containerEl);
